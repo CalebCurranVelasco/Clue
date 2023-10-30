a906f993c9546a8ea83eea7eaade96a1151c7788
CalebCurranVelasco <118134327+CalebCurranVelasco@users.noreply.github.com>
2023-10-30 13:30:29 -0600

    removed unnecessary code in DoorDirection

--- src/clueGame/DoorDirection.java
+++ src/clueGame/DoorDirection.java
@@ -12,13 +12,4 @@ package clueGame;
 
 public enum DoorDirection {
 	UP, DOWN, LEFT, RIGHT, NONE;
-//	private String value;
-//
-//	DoorDirection(String aValue) {
-//		value = aValue;
-//	}
-	
-//	public String toString() {
-//		return value;
-//	}
 }
