4fc5a2e0a0cd0bcaa46d8eb63cc21479d49a44a9
CalebCurranVelasco <118134327+CalebCurranVelasco@users.noreply.github.com>
2023-10-31 15:58:33 -0600

    added players and weapons to setup.txt and added erroring tests

---
+++ src/tests/GameSetupTests.java
@@ -0,0 +1,62 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clueGame.Board;
+
+class GameSetupTests {
+	
+	private static Board board;
+
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");		
+		// Initialize will load config files 
+		board.initialize();
+	}
+	
+	@Test
+	public void testPlayerColor() {
+		assertEquals("Gojo", board.getPlayer("Blue").getName());
+		assertEquals("Toji", board.getPlayer("Purple").getName());
+		assertEquals("Sukuna", board.getPlayer("Red").getName());
+		assertEquals("Geto", board.getPlayer("Green").getName());
+		assertEquals("Panda", board.getPlayer("White").getName());
+	}
+	
+	@Test
+	public void testWeaponLabels() {
+		assertTrue("Inverted Spear of Heaven", board.getWeapon("Inverted Spear of Heaven"));
+	}
+	
+	@Test
+	public void testHumanPlayer() {
+		assertEquals(board.getPlayer("Blue"), board.getHumanPlayer());
+		assertEquals(board.getPlayer("Red"), board.getComputerPlayer("Red"));
+	}
+	
+	@Test
+	void Cards() {
+		assertEquals(21, deck.size());
+	}
+	
+	@Test
+	void Solution() {
+		assertTrue(solution.getWeapon() != null);
+		assertTrue(solution.getPlayer() != null);
+		assertTrue(solution.getRoom() != null);
+	}
+	
+	@Test
+	void cardDealtToPlayer() {
+		assertTrue(player.getWeapon() != null);
+	}
+
+}
