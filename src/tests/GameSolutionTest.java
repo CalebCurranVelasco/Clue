bba2920bfe4fde45d17db0253d6a9815496b7775
Joshua Ramirez <jramirezmalerv@gmail.com>
2023-11-09 23:47:33 -0700

    Added comments to all tests in GameSolutionTest

--- src/tests/GameSolutionTest.java
+++ src/tests/GameSolutionTest.java
@@ -31,8 +31,12 @@ class GameSolutionTest {
 	
 	}
 
-	
-//	 Check accusation tests
+	 
+	/*
+	 * The test below tests the accusation. In this scenario, we set the solution to a 
+	 * specific person, room, and weapon card. We then create 4 cases where we check the 
+	 * accusation and only 1 of the 4 statements should match the exact solution set.
+	 */
 	@Test
 	void testAccusation() {
 		board.setSolution(new Card("Gojo", CardType.PERSON), new Card("Split Soul Katana", CardType.WEAPON), new Card("Dojo", CardType.ROOM));
@@ -42,6 +46,14 @@ class GameSolutionTest {
 		assertFalse(board.checkAccusation(new Card("Gojo", CardType.PERSON), new Card("Split Soul Katana", CardType.WEAPON), new Card("Temple", CardType.ROOM)));
 	}
 	
+	
+	
+	/*
+	 * The test below is a test for disproving a suggestion. The sscenario
+	 * below creates 2 cards and adds the cards to the player's (whose color is blue) hand.
+	 * We then create 2 scenarios in which the equals( ) method is called to compare the 
+	 * card types and ensure the name of the cards match.
+	 */
 	@Test
 	void disproveSuggestion() {
 		Card card = new Card("Dojo", CardType.ROOM);
@@ -53,6 +65,14 @@ class GameSolutionTest {
 		assertEquals(null, board.getPlayer("blue").disproveSuggestion(new Card("Geto", CardType.PERSON), new Card("Split Soul Katana", CardType.WEAPON), new Card("Temple", CardType.ROOM)));
 	}
 	
+	
+	/*
+	 * The test below tests the handling of a suggestion. In this test,
+	 * we create 6 cards and add a card to each players' hand.
+	 * We then create a test in which we use the equals( ) method to handle 
+	 * the suggestion. The handleSuggestion( ) function takes in a 
+	 * person, weapon, and room as well as the player handling the suggestion.
+	 */
 	@Test
 	void handleSuggesstion() {
 		Card card = new Card("Dojo", CardType.ROOM);
@@ -70,6 +90,15 @@ class GameSolutionTest {
 		assertTrue(card3.equals(board.handleSuggestion(card3, card4, card5, board.getPlayer("white"))));
 	}
 	
+	
+	/*
+	 * The test below creates a test for the case where only the player 
+	 * can disprove a suggestion. We create 8 cards below and give each player
+	 * a card in their hands. We are disproving the suggestion because nobody contains
+	 * the cards that are being handled in the suggestion, except the player whose
+	 * name corresponds with the color "blue."
+	 * 
+	 */
 	@Test
 	void onlyPlayerCanDisprove() {
 		Card card = new Card("Dojo", CardType.ROOM);
@@ -89,6 +118,10 @@ class GameSolutionTest {
 		assertEquals(null, board.handleSuggestion(card6, card7, card, board.getPlayer("blue")));
 	}
 	
+	/*
+	 * The test below works similar to the one above, except 
+	 * that no one can disprove since no cards match any of the players' hands.
+	 */
 	@Test
 	void noneCanDisprove() {
 		Card card = new Card("Dojo", CardType.ROOM);
@@ -109,6 +142,10 @@ class GameSolutionTest {
 		assertEquals(null, board.handleSuggestion(card6, card7, card8, board.getPlayer("red")));
 	}
 	
+	/*
+	 * The test below works similar to the one above, except 
+	 * that two players can disprove the suggestion.
+	 */
 	@Test
 	void twoCanDisprove() {
 		Card card = new Card("Dojo", CardType.ROOM);
