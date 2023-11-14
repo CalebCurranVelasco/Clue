ad68d168124aa82913de856c25b2eab6e58533c9
Joshua Ramirez <jramirezmalerv@gmail.com>
2023-11-14 15:11:28 -0700

    Created ClueGame class and BoardPanel class

---
+++ src/clueGame/ClueGame.java
@@ -0,0 +1,13 @@
+package clueGame;
+import javax.swing.JFrame;
+
+public class ClueGame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
